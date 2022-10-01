bfa2d5117511050bd312df57374ba88065643d5a
okayzzz27 <114280763+okayzzz27@users.noreply.github.com>
2022-10-01 16:47:37 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>tesla-website</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:300,400,600">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/Button-Modal-popup-team-member-Modal-popup-team-member.css">
+    <link rel="stylesheet" href="assets/css/Button-Modal-popup-team-member-styles.css">
+    <link rel="stylesheet" href="assets/css/Clients.css">
+    <link rel="stylesheet" href="assets/css/dh-row-text-image-right.css">
+    <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="assets/css/Google-Style-Login-.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css">
+    <link rel="stylesheet" href="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.css">
+    <link rel="stylesheet" href="assets/css/Pretty-Registration-Form-.css">
+    <link rel="stylesheet" href="assets/css/Single-Page-Contact-Us-Form.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body style="background: rgb(255,255,255);color: rgb(1,1,1);">
+    <section class="d-flex flex-column justify-content-between" id="hero" style="background: url(&quot;assets/img/bg1.png&quot;) center / cover no-repeat;">
+        <div id="hero-top">
+            <nav class="navbar navbar-light navbar-expand-md">
+                <div class="container-fluid">
+                    <p style="font-size: 20px;color: rgb(66,29,145);"><img width="95" height="53" style="width: 95px;height: 53px;background: url(&quot;assets/img/bittorrent-btt-logo.png&quot;);" src="assets/img/bittorrent-btt-logo.png"></p><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navcol-1">
+                        <ul class="navbar-nav mx-auto">
+                            <li class="nav-item"><a class="nav-link active" href="#">Home</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#video-and-stats">about us</a></li>
+                            <li class="nav-item"><a class="nav-link" href="#services">Services</a></li>
+                            <li class="nav-item"></li>
+                            <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                        </ul>
+                        <div id="Modal-button-wrapper-1" class="text-center"><a class="bs4_modal_trigger" href="" data-modal-id="bs4_team" data-toggle="modal" style="width: 112.825px;height: 40.5px;">
+                                <p style="background: #116cd9;border-radius: 24px;border-color: rgb(2,2,2);">LOGOUT</p>
+                            </a>
+                            <div id="bs4_team-1" class="modal fade bs4_modal bs4_blue bs4_bg_transp bs4_bd_black bs4_bd_semi_trnsp bs4_none_radius bs4_shadow_none bs4_center bs4_animate bs4FadeInDown bs4_duration_md bs4_easeOutQuint bs4_size_team" data-modal-backdrop="true" data-show-on="click" data-modal-delay="false" data-modal-duration="false">
+                                <div class="modal-dialog">
+                                    <div class="modal-content" style="/*background: var(--indigo);*/">
+                                        <div class="bs4_team_content bg-card1" style="background: rgba(255,255,255,0);">
+                                            <div class="login-card"><img class="profile-img-card" src="assets/img/avatar_2x.png">
+                                                <p class="profile-name-card"> </p>
+                                                <form class="form-inline form-signin"><span class="reauth-email"> </span><input class="form-control" type="email" id="inputEmail-1" required="" placeholder="Email address" autofocus=""><input class="form-control" type="password" id="inputPassword-1" required="" placeholder="Password">
+                                                    <div class="checkbox">
+                                                        <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-1"><label class="form-check-label" for="formCheck-1">Remember me</label></div>
+                                                    </div><button class="btn btn-primary btn-block btn-lg btn-signin" type="submit">Sign in</button><a href="registration.html">Create Account</a>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <div id="Modal-button-wrapper" class="text-center"><a class="bs4_modal_trigger" href="" data-modal-id="bs4_team" data-toggle="modal"><i class="fas fa-user" style="font-size: 21px;"></i></a>
+                                    <div id="bs4_team" class="modal fade bs4_modal bs4_blue bs4_bg_transp bs4_bd_black bs4_bd_semi_trnsp bs4_none_radius bs4_shadow_none bs4_center bs4_animate bs4FadeInDown bs4_duration_md bs4_easeOutQuint bs4_size_team" data-modal-backdrop="true" data-show-on="click" data-modal-delay="false" data-modal-duration="false">
+                                        <div class="modal-dialog">
+                                            <div class="modal-content" style="/*background: var(--indigo);*/">
+                                                <div class="bs4_team_content bg-card1" style="background: rgba(255,255,255,0);">
+                                                    <div class="login-card"><img class="profile-img-card" src="assets/img/avatar_2x.png">
+                                                        <p class="profile-name-card"> </p>
+                                                        <form class="form-inline form-signin"><span class="reauth-email"> </span><input id="inputEmail" class="form-control" type="email" required placeholder="Username" autofocus /><input class="form-control" type="password" id="inputPassword" required="" placeholder="Password">
+                                                            <div class="checkbox">
+                                                                <div class="form-check"><input class="form-check-input" type="checkbox" id="formCheck-3"><label class="form-check-label" for="formCheck-3">Remember me</label></div>
+                                                            </div><button class="btn btn-primary btn-block btn-lg btn-signin" type="submit">Sign in</button><a href="registration.html">Create Account</a>
+                                                        </form>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <h2 class="text-center" data-aos="fade-up" data-aos-duration="700" data-aos-once="true" id="subtitle" style="margin-bottom: 104px;padding-top: 128px;padding-bottom: 128px;color: var(--light);font-weight: bold;font-family: Montserrat, sans-serif;">OLSHCU</h2>
+        </div>
+    </section>
+    <section data-aos="fade-up" data-aos-duration="700" id="video-and-stats" style="padding-top: 66px;padding-bottom: 66px;">
+        <h1 style="padding-bottom: 0px;margin-bottom: 46px;">DASHBOARD</h1>
+        <div class="container">
+            <!-- TradingView Widget BEGIN -->
+<div class="tradingview-widget-container">
+    <div class="tradingview-widget-container__widget"></div>
+    <div class="tradingview-widget-copyright"><a href="https://www.tradingview.com/markets/cryptocurrencies/prices-all/" rel="noopener" target="_blank"><span class="blue-text">Cryptocurrency Markets</span></a> by TradingView</div>
+    <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-screener.js" async>
+    {
+    "width"; 1000,
+    "height"; 490,
+    "defaultColumn"; "overview",
+    "screener_type"; "crypto_mkt",
+    "displayCurrency"; "BTC",
+    "colorTheme"; "light",
+    "locale"; "en",
+    "isTransparent"; false
+  }
+    </script>
+  </div>
+          </div></div>
+    </section>
+    <section>
+        <aside id="section-clients" class="clients">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3"><a href="#"> <img class="img-fluid" src="assets/img/envato.jpg"></a></div>
+                    <div class="col-sm-6 col-md-3"><a href="#"> <img class="img-fluid" src="assets/img/designmodo.jpg"></a></div>
+                    <div class="col-sm-6 col-md-3"><a href="#"> <img class="img-fluid" src="assets/img/themeforest.jpg"></a></div>
+                    <div class="col-sm-6 col-md-3"><a href="#"> <img class="img-fluid" src="assets/img/creative-market.jpg"></a></div>
+                </div>
+            </div>
+        </aside>
+    </section>
+    <section></section>
+    <div class="footer-basic">
+        <footer data-aos="fade-up" data-aos-duration="700">
+            <div id="vertical-line"></div>
+            <ul class="list-inline">
+                <li class="list-inline-item"><a href="#">reserved© 2022</a></li>
+                <li class="list-inline-item"><a href="#">Privacy &amp; Legal</a></li>
+                <li class="list-inline-item"><a href="#">Contact </a></li>
+                <li class="list-inline-item"><a href="#">Careers </a></li>
+                <li class="list-inline-item"><a href="#">Forums </a></li>
+                <li class="list-inline-item"><a href="#">Locations </a></li>
+                <li class="list-inline-item"><a href="#">United States</a></li>
+            </ul>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.js"></script>
+    <script src="assets/js/Button-Modal-popup-team-member.js"></script>
+    <script src="https://unpkg.com/@bootstrapstudio/bootstrap-better-nav/dist/bootstrap-better-nav.min.js"></script>
+</body>
+
+</html>
